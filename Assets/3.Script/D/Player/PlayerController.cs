875b93fd68b429170c7033400c706302060ad8b5
Jeong Minchan <minchan3465@gmail.com>
2025-12-16 10:51:51 +0900

    플레이어 구조 로직 완성
    플레이어 인풋을 Event로 변경했습니다.
    조금 더 유지보수가 쉽고, 활용처가 넒어졌습니다.

--- Assets/3.Script/D/Player/PlayerController.cs
+++ Assets/3.Script/D/Player/PlayerController.cs
@@ -1,40 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
 
 public class PlayerController : MonoBehaviour {
 	//플레이어 컨트롤러로 이름을 바꿔야할듯.
 	private Rigidbody playerRB;
+	[Header("인풋 컴포넌트 추가!")]
+	[SerializeField] private PlayerInput input;
 
 	[Header("플레이어 이동속도")]
-	public float movementSpeed = 0f;
-	private float walkSpeed = 150f;
-	private float runSpeed = 400;
-
-	private Vector2 direction = Vector2.zero;
+	public float movementSpeed;
+	[SerializeField] private float walkSpeed;
+	[SerializeField] private float runSpeed;
 
 	[Header("플레이어 점프력")]
-	public float jumpPower = 0f;
+	[SerializeField] private float jumpPower;
 
 	[Header("플레이어 감도")]
-	public float sensitive = 0f;
+	//추후 설정에서 감도 조절을 할 수 있어야 하기에, public으로 구현
+	public float sensitive;
 	[Header("카메라 오브젝트")]
 	[SerializeField] private Camera cameraObject;
 
-	private Vector2 mouseDelta;
 	private float currentY;
 	private float rotateBoundary = 80f;
 
+	//컴포넌트 설정
 	private void Awake() {
 		TryGetComponent(out playerRB);
-		movementSpeed = walkSpeed;
+		Walk();
 	}
-
+	//이벤트 등록
+	private void Start() {
+		input.onJump += Jump;
+		input.onRun += Run;
+		input.onWalk += Walk;
+	}
+	//움직임
 	private void FixedUpdate() {
 		Move();
 	}
-
+	//시야
 	private void LateUpdate() {
 		Rotate();
 	}
@@ -42,46 +50,42 @@ public class PlayerController : MonoBehaviour {
 	private void Move() {
 		//플레이어 앞뒤(WS/Z축) + 좌우(AD/X축)에 이동속도 곱하기.
 		//y값은 플레이어 점프 현황 물리 속도에 맞춰서 저장 (안하면 엄청 느리게 떨어짐)
-		Vector3 playerMoveVector = (transform.forward * direction.y + transform.right * direction.x) * movementSpeed * Time.deltaTime;
+		Vector3 playerMoveVector = (transform.forward * input.direction.y + transform.right * input.direction.x);
+		playerMoveVector = Calc_Speed(playerMoveVector);
 		playerMoveVector.y = playerRB.linearVelocity.y;
 		playerRB.linearVelocity = playerMoveVector;
 	}
 	private void Rotate() {
 		//플레이어 위아래 시점에 감도값 곱.
-		currentY += mouseDelta.y * sensitive;
+		currentY += input.mouseDelta.y * sensitive;
 		//시점 제한(위아래)
 		float newY = Mathf.Clamp(currentY, -rotateBoundary, rotateBoundary);
 
 		//카메라 시점 변경
 		//플레이어 좌우회전
 		cameraObject.transform.localEulerAngles = new Vector3(-newY, 0, 0);
-		transform.eulerAngles += new Vector3(0, mouseDelta.x * sensitive, 0);
+		transform.eulerAngles += new Vector3(0, input.mouseDelta.x * sensitive, 0);
+	}
+	private void Jump() {
+		playerRB.linearVelocity = Vector3.zero;
+		playerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+	}
+	private void Run() {
+		movementSpeed = runSpeed;
+	}
+	private void Walk() {
+		movementSpeed = walkSpeed;
 	}
 
-	//플레이어 무브
-	public void OnMove(InputAction.CallbackContext context) {
-		if (context.phase.Equals(InputActionPhase.Performed)) {
-			direction = context.ReadValue<Vector2>();
-		} else if (context.phase.Equals(InputActionPhase.Canceled)) {
-			direction = Vector2.zero;
-		}
+	[Header("테스트")]
+	[SerializeField] private float inventory;
+	[SerializeField] private float max_inventory;
+
+	private Vector3 Calc_Speed(Vector3 vector) {
+		//총 무게 가져오기
+		float Speed = movementSpeed * (1 - (inventory / max_inventory));
+		vector *= Speed * Time.deltaTime;
+		return vector;
 	}
-	//플레이어 점프
-	public void OnJump(InputAction.CallbackContext context) {
-		if (context.phase.Equals(InputActionPhase.Started)) {
-			playerRB.linearVelocity = Vector3.zero;
-			playerRB.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-		}
-	}
-	//플레이어 시야 회전
-	public void OnRotate(InputAction.CallbackContext context) {
-		mouseDelta = context.ReadValue<Vector2>();
-	}
-	public void OnRun(InputAction.CallbackContext context) {
-		if (context.phase.Equals(InputActionPhase.Performed)) {
-			movementSpeed = runSpeed;
-		} else if (context.phase.Equals(InputActionPhase.Canceled)) {
-			movementSpeed = walkSpeed;
-		}
-	}
+
 }
---
+++ Assets/3.Script/D/Player/PlayerInput.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour {
+	//public Action onMove;
+	//public Action onLook;
+	public Action onJump;
+	public Action onRun;
+	public Action onWalk;
+	public Action onPickUp;
+	public Action onInteract;
+
+	public Vector2 direction = Vector2.zero;
+	public Vector2 mouseDelta = Vector2.zero;
+
+	//플레이어 움직임
+	public void Event_Move(InputAction.CallbackContext context) {
+		if (context.phase.Equals(InputActionPhase.Performed)) {
+			direction = context.ReadValue<Vector2>();
+		} else if (context.phase.Equals(InputActionPhase.Canceled)) {
+			direction = Vector2.zero;
+		}
+	}
+	//플레이어 시야 회전
+	public void Event_Look(InputAction.CallbackContext context) {
+		mouseDelta = context.ReadValue<Vector2>();
+	}
+	//플레이어 점프
+	public void Event_Jump(InputAction.CallbackContext context) {
+		if (context.phase.Equals(InputActionPhase.Started)) {
+			onJump();
+		}
+	}
+	//플레이어 달리기
+	public void Event_Run(InputAction.CallbackContext context) {
+		if (context.phase.Equals(InputActionPhase.Started)) {
+			onRun();
+		} else if (context.phase.Equals(InputActionPhase.Canceled)) {
+			onWalk();
+		}
+	}
+	//플레이어 좌클릭(줍기)
+	public void Event_PickUp(InputAction.CallbackContext context) {
+		if (context.phase.Equals(InputActionPhase.Started)) {
+			onPickUp();
+		}
+	}
+	//플레이어 상호작용
+	public void Event_Interact(InputAction.CallbackContext context) {
+		if (context.phase.Equals(InputActionPhase.Started)) {
+			onInteract();
+		}
+	}
+}
