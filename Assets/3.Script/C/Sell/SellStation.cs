ba37f3aaf4ba1b9ab4efef8c5ba5fd18eee65266
Changsikeee <skagns199511@gmail.com>
2025-12-18 15:19:57 +0900

    업그레이드, 샤워, 정산 이벤트로 전환 및 속도 제거

--- Assets/3.Script/C/Sell/SellStation.cs
+++ Assets/3.Script/C/Sell/SellStation.cs
@@ -7,41 +7,58 @@ public class SellStation : MonoBehaviour
     [SerializeField] private GameObject guideText; // "F키를 눌러 상점 열기" 텍스트 오브젝트
 
     private bool isPlayerNearby = false;
+    private PlayerInput targetInput;
 
     private void Start()
     {
         if (guideText != null) guideText.SetActive(false);
     }
 
-    private void Update()
+    private void TryOpenShop()
     {
-        // 플레이어가 범위 안에 있고 F를 누르면 UI 오픈
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.F))
-        {
-            if (sellUI != null) sellUI.OpenSellMenu();
-        }
+        if (sellUI != null) sellUI.OpenSellMenu();
     }
 
-    // Trigger 영역 진입 감지
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerNearby = true;
+            // 1. 안내 문구 켜기
             if (guideText != null) guideText.SetActive(true);
+
+            // 2. 플레이어의 입력 스크립트(PlayerInput) 가져오기
+            targetInput = other.GetComponent<PlayerInput>();
+
+            // 3. 이벤트 구독 (이제 F키 누르면 TryOpenShop이 실행됨)
+            if (targetInput != null)
+            {
+                targetInput.onInteract += TryOpenShop;
+            }
         }
     }
 
     // Trigger 영역 이탈 감지
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-            if (guideText != null) guideText.SetActive(false);
+        // 안내 문구 끄기
+        if (guideText != null) guideText.SetActive(false);
 
-            // 멀어지면 상점 UI 강제 종료 (안전장치)
-            if (sellUI != null) sellUI.CloseAllPanels();
+        // 이벤트 구독 해제 (안 하면 멀리서도 상점이 열림)
+        if (targetInput != null)
+        {
+            targetInput.onInteract -= TryOpenShop;
+            targetInput = null; // 참조 비우기
+        }
+
+        // 3. 상점 UI 강제 종료
+        if (sellUI != null) sellUI.CloseAllPanels();
+    }
+    private void OnDisable()
+    {
+        if (targetInput != null)
+        {
+            targetInput.onInteract -= TryOpenShop;
+            targetInput = null;
         }
     }
 }
