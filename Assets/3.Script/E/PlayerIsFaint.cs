b823587eb7fb10d0841c2404332003d10f68b82b
PurpleCloud-0114 <purple.cloud.010203@gmail.com>
2025-12-22 13:15:56 +0900

    1315_update
    conect 남훈님

---
+++ Assets/3.Script/E/PlayerIsFaint.cs
@@ -0,0 +1,11 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIsFaint : MonoBehaviour
+{
+    //플레이어의 청결도가 0 이하로 떨어지면 발생
+    //카메라를 회전시키며 바닥으로 이동시킴
+}
