0617a8813727bfa80557a8038989dc456f5e95e0
Jeong Minchan <minchan3465@gmail.com>
2025-12-22 09:48:52 +0900

    쓰레기 아웃라인 메소드 추가

--- Assets/3.Script/E/SmallTrash.cs
+++ Assets/3.Script/E/SmallTrash.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SmallTrash : MonoBehaviour
@@ -10,5 +11,24 @@ public class SmallTrash : MonoBehaviour
     public int getTrashNum()
     {
         return smallTrashNum;
+    }
+
+    [SerializeField] private Material outline_material;
+    private Renderer render;
+
+    private void Start() {
+        TryGetComponent(out render);
+    }
+
+    public void onOutline() {
+        List<Material> materials = render.sharedMaterials.ToList();
+        materials.Add(outline_material);
+        render.materials = materials.ToArray();
+    }
+
+    public void offOutline() {
+        List<Material> materials = render.sharedMaterials.ToList();
+        materials.Remove(outline_material);
+        render.materials = materials.ToArray();
     }
 }
