21f5195083c4b8e917648b8b6641357359b8df19
PurpleCloud-0114 <purple.cloud.010203@gmail.com>
2025-12-22 17:38:25 +0900

    faint update

--- Assets/3.Script/E/PlayerIsFaint.cs
+++ Assets/3.Script/E/PlayerIsFaint.cs
@@ -7,13 +7,14 @@ using UnityEngine;
 public class PlayerIsFaint : MonoBehaviour
 {
     [SerializeField] private float fallDuration = 1.5f;
-    [SerializeField] private Vector3 fallPositionOffset = new Vector3(0, -0.5f, 0.5f); // 바닥으로 내려가고 살짝 앞으로
+    [SerializeField] private Vector3 fallPositionOffset = new Vector3(0, 0f, 0.5f); // 바닥으로 내려가고 살짝 앞으로
     [SerializeField] private Vector3 fallRotationOffset = new Vector3(10f, 0, 85f);   // 옆으로 픽 쓰러지는 각도 (Z축 중요)
     [SerializeField] private AnimationCurve fallCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     //[SerializeField] private ShowerSystem showerSystem;
     [SerializeField] private GameObject Sceneload_fade;
     [SerializeField] private VignetteController vignetteController;
     [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private Camera camera;
     public void StartPassOutEffect()
     {
         DoNotControl(true);
@@ -30,12 +31,12 @@ public class PlayerIsFaint : MonoBehaviour
         Quaternion targetRot = Quaternion.Euler(fallRotationOffset);
 
         float elapsed = 0f;
+        DoNotControl(true);
         while (elapsed < fallDuration)
         {
             elapsed += Time.deltaTime;
             float t = fallCurve.Evaluate(elapsed / fallDuration);
 
-            // 카메라 위치와 회전을 서서히 변경
             transform.localPosition = Vector3.Lerp(startPos, targetPos, t);
             transform.localRotation = Quaternion.Slerp(startRot, targetRot, t);
             yield return null;
@@ -45,17 +46,19 @@ public class PlayerIsFaint : MonoBehaviour
 
         Debug.Log("연출 끝: 리스폰 로직 실행");
         //showerSystem.TryShower();
-        yield return new WaitForSeconds(2f);
         Sceneload_fade.SetActive(true);
+        yield return new WaitForSeconds(1f);
         HandleResetPlayer();
         playerInput.onPickUp();
+        DoNotControl(false);
     }
 
     private void HandleResetPlayer()
     {
         transform.localPosition = new Vector3(-1.22f, -0.69f, -13.496f);
-        transform.localRotation = Quaternion.identity;
-        GameManager.instance.ChangeHP(GameManager.instance.P_MaxHP);
+        transform.localRotation = Quaternion.Euler(0,-90f,0);
+        camera.transform.rotation = Quaternion.Euler(0, 0, 0);
+        GameManager.instance.P_CurrentHP = GameManager.instance.P_MaxHP;
         GameManager.instance.P_Weight = 0;
         vignetteController.OnWash();
     }
