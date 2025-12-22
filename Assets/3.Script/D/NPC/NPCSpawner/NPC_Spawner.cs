cbd4af0ec16dd3df2e116c1db0b38ed6361bbfde
Jeong Minchan <minchan3465@gmail.com>
2025-12-22 15:15:31 +0900

    길 제작 완성

--- Assets/3.Script/D/NPC/NPCSpawner/NPC_Spawner.cs
+++ Assets/3.Script/D/NPC/NPCSpawner/NPC_Spawner.cs
@@ -7,7 +7,6 @@ public class NPC_Spawner : MonoBehaviour {
 	[SerializeField] protected GameObject NPC;
 
 	[Header("이동 경로 Vector")]
-	[SerializeField] private List<Transform> spawn_locations;
 	protected Transform start_pos;
 	protected Transform end_pos;
 
@@ -17,15 +16,15 @@ public class NPC_Spawner : MonoBehaviour {
 	protected WaitForSeconds seconds;
 
 	protected void Rnd_Set_Pos() {
-		List<Transform> copy_locations_list = new List<Transform>();
-		int copy_count = spawn_locations.Count;
-		for(int i = 0; i < copy_count; i++) {
-			copy_locations_list.Add(spawn_locations[i]);
+		List<Transform> locations_list = new List<Transform>();
+		for(int i = 0; i < transform.childCount; i++) {
+			locations_list.Add(transform.GetChild(i));
 		}
-		int rad_val = Random.Range(0, copy_locations_list.Count);
-		start_pos = copy_locations_list[rad_val];
-		copy_locations_list.RemoveAt(rad_val);
-		rad_val = Random.Range(0, copy_locations_list.Count);
-		end_pos = copy_locations_list[rad_val];
+
+		int rad_val = Random.Range(0, locations_list.Count);
+		start_pos = locations_list[rad_val];
+		locations_list.RemoveAt(rad_val);
+		rad_val = Random.Range(0, locations_list.Count);
+		end_pos = locations_list[rad_val];
 	}
 }
