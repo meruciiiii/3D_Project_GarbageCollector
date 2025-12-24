4b89a3adb00b658c2f89cf4c1d4d1947fc359455
meruciiiii <127821538+meruciiiii@users.noreply.github.com>
2025-12-24 10:48:53 +0900

    메모리 누수잡기 범인은 찾았는데 어케 수정하지

--- Assets/3.Script/D/NPC/NPCSpawner/Stage3_Spawner.cs
+++ Assets/3.Script/D/NPC/NPCSpawner/Stage3_Spawner.cs
@@ -10,18 +10,31 @@ public class Stage3_Spawner : NPC_Spawner {
 	private IEnumerator NPC_Spawn_co() {
 		while (true) {
 			seconds = new WaitForSeconds(Random.Range(min_sec, max_sec));
-			if (npc_pooling[pool_current].activeSelf) {
-				NPC_Pooling_ReSize();
-				pool_current = pool_count - 1;
+			
+			int inactiveNpcIndex = -1;
+			for (int i = 0; i < pool_count; i++)
+			{
+				if (!npc_pooling[i].activeSelf)
+				{
+					inactiveNpcIndex = i;
+					break;
+				}
 			}
+
+			if (inactiveNpcIndex == -1)
+			{
+				NPC_Pooling_ReSize();
+				inactiveNpcIndex = pool_count - 1;
+			}
+
+			pool_current = inactiveNpcIndex;
+			
 			GameObject victim = npc_pooling[pool_current];
 			victim.TryGetComponent(out Stage3_Victim victim_pattern);
 			Rnd_Set_Pos();
 			victim_pattern.set_pos(start_pos.position, end_pos.position);
 			victim.SetActive(true);
 			victim_pattern.start();
-			pool_current++;
-			if (pool_current.Equals(pool_count)) { pool_current = 0; }
 			yield return seconds;
 		}
 	}
