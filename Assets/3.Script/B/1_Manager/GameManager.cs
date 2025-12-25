256d17cffe24e72175e536d29f969306119ed2e2
Jeong Minchan <minchan3465@gmail.com>
2025-12-25 15:30:43 +0900

    Zone 누락된거 수정 (1/2)

--- Assets/3.Script/B/1_Manager/GameManager.cs
+++ Assets/3.Script/B/1_Manager/GameManager.cs
@@ -35,6 +35,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public int Current_Area = 1;
+
     public void ChangeHP(int HPindecrease)
     {
         P_CurrentHP = Mathf.Clamp(P_CurrentHP + HPindecrease, 0, P_MaxHP);//최대 최소 체력 제한
