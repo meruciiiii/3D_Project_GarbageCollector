d722babd21e636eff48d4d6102c42c0b30300f7a
Jeong Minchan <minchan3465@gmail.com>
2025-12-26 19:12:29 +0900

    NPC 랜덤 외형 50%

--- Assets/3.Script/D/NPC/NPC/NPC_Base.cs
+++ Assets/3.Script/D/NPC/NPC/NPC_Base.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class NPC_Base : MonoBehaviour {
-	//쓰레기 생성 컴포넌트
-	[HideInInspector] public NPC_Create_Trash npc_create_trash;
-
-
-	[Header("범죄 확률")]
+[Header("범죄 확률")]
 	[Range(0,100)] public float percent;
 
 	[Header("이동 속도")]
@@ -19,20 +15,17 @@ public class NPC_Base : MonoBehaviour {
 	public Vector3 start_pos;
 	public Vector3 end_pos;
 
-	[Header("AreaManager")]
-	[SerializeField] protected AreaManager areaManager;
-
-	//NavMesh Agent
+	//[Header("컴포넌트 등록")]
+	[HideInInspector] public NPC_Random_Mesh npc_random_mesh;
+	[HideInInspector] public NPC_Create_Trash npc_create_trash;
 	[HideInInspector] public NavMeshAgent agent;
 
 	//State Pattern
 	public IState currentState;
-
 	public IState spawnState;
 	public IState moveState;
 	public IState setPosState;
 
-	// ---
 	//구역 이벤트 수신용
 	[SerializeField] protected bool isActive;
 
@@ -41,6 +34,7 @@ public class NPC_Base : MonoBehaviour {
 	protected virtual void Awake() {
 		TryGetComponent(out agent);
 		TryGetComponent(out npc_create_trash);
+		TryGetComponent(out npc_random_mesh);
 		agent.speed = NPC_speed;
 
 		spawnState = new SpawnState(this);
