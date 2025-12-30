dc968e6e3970e5aabd1c58a56e60711721b90721
Jeong Minchan <minchan3465@gmail.com>
2025-12-30 11:29:30 +0900

    디버그 삭제 / JSON 초기화

--- Assets/3.Script/B/2_data/Json/JsonDataManger.cs
+++ Assets/3.Script/B/2_data/Json/JsonDataManger.cs
@@ -31,7 +31,7 @@ public static class JsonDataManger
         string Jsondata = JsonUtility.ToJson(data);
 
         File.WriteAllText(path, Jsondata); //경로 파일 안에 json파일로 저장
-        Debug.Log("Playerdata Wirte완료");
+        //Debug.Log("Playerdata Wirte완료");
     }
 
     public static PlayerData LoadformJson()//데이터 불러오기 메서드
