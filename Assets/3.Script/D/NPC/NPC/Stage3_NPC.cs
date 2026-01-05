e09b378ee624d0f3136cc2ebf2ca321916a77fb4
Jeong Minchan <minchan3465@gmail.com>
2026-01-05 15:10:35 +0900

    NPC 차량 충돌 다시 추가

--- Assets/3.Script/D/NPC/NPC/Stage3_NPC.cs
+++ Assets/3.Script/D/NPC/NPC/Stage3_NPC.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Stage3_NPC : NPC_Base {
-	[Header("쓰레기 드랍 횟수")]
+	[HideInInspector] public IState becomeTrashState;
+
+	[Header("쓰레기 드랍 갯수")]
 	[SerializeField] private float max_drop_cnt = 3;
 	public float drop_cnt = 0;
 
@@ -11,21 +13,25 @@ public class Stage3_NPC : NPC_Base {
 	[SerializeField] private float min_sec = 3;
 	[SerializeField] private float max_sec = 6;
 
-	//area 판정용
+	[Header("충돌 관련")]
+	public bool isClone = false;
+
+	//area 판정 확인
 	public bool inArea = false;
 
 	private Transform area_object;
 
 	protected override void Awake() {
 		base.Awake();
+		becomeTrashState = new BecomeTrashState(this);
 		area_object = GameObject.FindGameObjectWithTag("Area03").transform;
 		npc_create_trash.area = area_object;
 	}
 
 
-	//이걸 조절하는건 
-	public void run_coroutine() {StartCoroutine(drop_trash());}
-	public void stop_coroutine() {StopCoroutine(drop_trash());}
+	//코루틴 판정
+	public void run_coroutine() { StartCoroutine(drop_trash()); }
+	public void stop_coroutine() { StopCoroutine(drop_trash()); }
 	private IEnumerator drop_trash() {
 		while (drop_cnt < max_drop_cnt) {
 			float maxTime = Random.Range(min_sec, max_sec);
@@ -42,7 +48,33 @@ public class Stage3_NPC : NPC_Base {
 		}
 	}
 	protected override void Event_ChangeArea(int area) {
-		if (area.Equals(3)) { isActive = true; } 
-		else { isActive = false; }
+		if (area.Equals(3)) { isActive = true; } else { isActive = false; }
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Car") && !isClone && inArea && GameManager.instance.Current_Area.Equals(3)) {
+			CloneTrashRuntime(other.transform.position);
+		}
+	}
+
+	private void CloneTrashRuntime(Vector3 car_pos) {
+		GameObject cloneGO = Instantiate(gameObject);
+		if (cloneGO.TryGetComponent(out Trash cloneTrash)) {
+			CloneSetting(cloneTrash);
+			cloneTrash.isRuntimeInstance = true;
+		}
+		if (cloneGO.TryGetComponent(out Stage3_NPC npc_script)) {
+			npc_script.isClone = true;
+		}
+		inArea = false;
+		ChangeState(setPosState);
+	}
+
+	private void CloneSetting(Trash cloneTrash) {
+		if (cloneTrash.TryGetComponent(out UnityEngine.AI.NavMeshAgent Nav)) { Nav.enabled = false; }
+		if (cloneTrash.TryGetComponent(out Stage3_NPC stageNpc)) { stageNpc.enabled = false; }
+		if (cloneTrash.TryGetComponent(out NPC_Create_Trash create_Trash)) { create_Trash.enabled = false; }
+		if (cloneTrash.TryGetComponent(out NPC_Random_Mesh random_Mesh)) { random_Mesh.enabled = false; }
+		if (cloneTrash.TryGetComponent(out Animator animator)) { animator.enabled = false; }
 	}
 }
